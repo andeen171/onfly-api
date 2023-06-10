125803040292eeea6f3f7c18d7dd9e8c948c735a
Anderson Lopes <zeander21@outlook.com.br>
2023-06-10 20:23:26 -0300

    Expenses base crud

--- app/Http/Controllers/API/ExpenseController.php
+++ app/Http/Controllers/API/ExpenseController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ExpenseResource;
+use App\Models\Expense;
 use Illuminate\Http\Request;
 
 class ExpenseController extends Controller
@@ -12,7 +14,9 @@ class ExpenseController extends Controller
      */
     public function index()
     {
-        //
+        // Return all expenses associated with the authenticated user.
+        // return ExpenseResource::collection(auth()->user()->expenses);
+        return ExpenseResource::collection(Expense::all());
     }
 
     /**
@@ -20,30 +24,47 @@ class ExpenseController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'description' => 'required|string|max:191',
+            'date' => 'required|date|before_or_equal:today',
+            'value' => 'required|numeric|min:0',
+        ]);
+
+        $expense = Expense::create($validatedData);
+        // Associate the expense with the authenticated user.
+        // $expense->user()->associate(auth()->user());
+        return new ExpenseResource($expense);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Expense $expense)
     {
-        //
+        return new ExpenseResource($expense);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Expense $expense)
     {
-        //
+        $validatedData = $request->validate([
+            'description' => 'required|string|max:191',
+            'date' => 'required|date|before_or_equal:today',
+            'value' => 'required|numeric|min:0',
+        ]);
+
+        $expense->update($validatedData);
+        return new ExpenseResource($expense);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Expense $expense)
     {
-        //
+        $expense->delete();
+        return response()->noContent();
     }
 }
--- database/migrations/2023_06_10_224909_create_expenses_table.php
+++ database/migrations/2023_06_10_224909_create_expenses_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
+            $table->date('date');
+            $table->decimal('value', 8, 2);
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
